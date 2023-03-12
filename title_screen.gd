7ad25b8abdc81cf5a54da5a1402a521f11ac1447
redxaxder <abyaly@gmail.com>
2023-03-12 13:54:08 -0500

    fix fix

--- title_screen.gd
+++ title_screen.gd
@@ -7,7 +7,8 @@ func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept"):	
 		sfx.end_song()
 # warning-ignore:return_value_discarded
-		sfx.connect("song_ended",self, "_transition", [],CONNECT_ONESHOT | CONNECT_DEFERRED)
+		if !sfx.is_connected("song_ended", self, "_transition"):
+			sfx.connect("song_ended",self, "_transition", [],CONNECT_ONESHOT | CONNECT_DEFERRED)
 
 func _transition():
 	# warning-ignore:return_value_discarded
--- ui/bubbly.gd
+++ ui/bubbly.gd
@@ -33,6 +33,7 @@ func _process(delta):
 	for c in cs:
 		c.rect_position.y = max(c.rect_position.y - dy, prev_bottom)
 		c.rect_size.x = rect_size.x - 5
+		c.rect_size.y = 0
 		prev_bottom = c.rect_position.y + spacing + c.rect_size.y
 		if !c.get("fading"):
 			height += c.rect_size.y + spacing
