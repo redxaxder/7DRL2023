f45836f6ad2743f5f20aafddc2da61441f485524
redxaxder <abyaly@gmail.com>
2023-03-12 13:36:43 -0500

    debork log

--- ui/log.gd
+++ ui/log.gd
@@ -9,9 +9,9 @@ export var left_margin: int = 5
 
 func _on_write_log(log_text: String):
 	var new_entry = Label.new()
+	new_entry.rect_size.x = rect_size.x
+	new_entry.autowrap = true
 	new_entry.visible = true
 	new_entry.text = log_text
-	new_entry.autowrap = true
-	new_entry.rect_size.x = rect_size.x
 	new_entry.rect_position = Vector2(left_margin, -15)
 	bubbler.spawn(new_entry)
